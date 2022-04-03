083e70b1af75c8f3c226032d6a6e6954427db714
gaurav <guravvala492@gmail.com>
2022-04-03 20:43:39 +0530

    ui changes

--- sctipts/app.js
+++ sctipts/app.js
@@ -1,17 +1,5 @@
-// LOGIN PAGE
-let username = document.querySelector(".uname");
-let password = document.querySelector(".pass");
-let submit = document.querySelector(".submit");
+// INDEX PAGE
 
-console.log("hello");
-submit.addEventListener("click", (e) => {
-  if (username.value === "gaurav" && password.value === "gav") {
-    console.log("Correct");
-    window.location.href = "index.html";
-  } else {
-    alert("wrong credentials");
-    // window.location.href = "login.html";
-  }
-
-  e.preventDefault();
+tippy("#home", {
+  content: "My tooltip!",
 });
